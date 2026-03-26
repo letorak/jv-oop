0ab7d70949510c173d5a9baf3e795edead1aa2ba
ddmay <dneprgeostart@gmail.com>
2026-03-26 11:31:06 +0200

    finish  jv-oop

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        // Створюємо масив типу Machine і кладемо туди наші об'єкти
+        Machine[] machines = new Machine[] {
+                new Truck(),
+                new Bulldozer(),
+                new Excavator()
+        };
 
+        // Проходимо циклом по кожній машині
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
